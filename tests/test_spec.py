e18be1971bf1b5171aec6d2cf0878c70a6cef18b
Álvaro Bartolomé <alvarob96@usal.es>
2022-06-27 21:17:32 +0000

    🧪 Add `WandbFileSystem` init & `ls` tests

---
+++ tests/test_spec.py
@@ -0,0 +1,25 @@
+# Copyright 2022 Alvaro Bartolome, alvarobartt @ GitHub
+# See LICENSE for details.
+
+from typing import List
+
+import pytest
+
+from wandbfs.spec import WandbFileSystem
+
+
+class TestWandbFileSystem:
+    """Test `wandbfs.spec.WandbFileSystem` class methods."""
+
+    @pytest.fixture(autouse=True)
+    @pytest.mark.usefixtures("entity", "project", "run_id")
+    def setup_method(self, entity: str, project: str, run_id: str) -> None:
+        self.fs = WandbFileSystem(entity=entity, project=project, run_id=run_id)
+
+    def teardown(self):
+        del self.fs
+
+    def test_ls(self) -> None:
+        """Test `WandbFileSystem.ls` method."""
+        files = self.fs.ls(path="./media/graph")
+        assert isinstance(files, List)
