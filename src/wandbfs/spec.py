72bd700fe4ee50197836ed1cfb642f090b6a064f
Álvaro Bartolomé <alvarob96@usal.es>
2022-06-27 21:15:30 +0000

    ✨ Define `WandbFileSystem` init

--- src/wandbfs/spec.py
+++ src/wandbfs/spec.py
@@ -3,22 +3,34 @@
 
 import os
 
+import wandb
 from fsspec import AbstractFileSystem
 
 
 class WandbFileSystem(AbstractFileSystem):
     protocol = "wandb"
 
-    def __init__(self, api_key: str = None) -> None:
+    def __init__(
+        self,
+        api_key: str = None,
+        entity: str = None,
+        project: str = None,
+        run_id: str = None,
+    ) -> None:
         super().__init__()
 
         if api_key:
             os.environ["WANDB_API_KEY"] = api_key
 
         assert os.getenv("WANDB_API_KEY") is None, (
-            "In order to connect to the wandb Public API you need to provide the"
-            " API key either via param `api_key` or setting the key in the"
-            " environment variable `WANDB_API_KEY`."
+            "In order to connect to the wandb Public API you need to provide the API"
+            " key either via param `api_key`, setting the key in the environment"
+            " variable `WANDB_API_KEY`, or running `wandb login <WANDB_API_KEY>`."
         )
 
         self.api = wandb.Api()
+        self._run = None
+
+        self.entity = entity
+        self.project = project
+        self.run_id = run_id
