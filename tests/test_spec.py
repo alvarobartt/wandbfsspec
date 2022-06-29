bcadbf90ee6237b26669db0e70cbc83663dcf82b
Álvaro Bartolomé <alvarob96@usal.es>
2022-06-29 07:51:42 +0000

    🧪 Fix setup method of `WandbFileSystem`

--- tests/test_spec.py
+++ tests/test_spec.py
@@ -14,9 +14,8 @@ class TestWandbFileSystem:
     """Test `wandbfs.spec.WandbFileSystem` class methods."""
 
     @pytest.fixture(autouse=True)
-    @pytest.mark.usefixtures("entity", "project", "run_id")
-    def setup_method(self, entity: str, project: str, run_id: str) -> None:
-        self.fs = WandbFileSystem(entity=entity, project=project, run_id=run_id)
+    def setup_method(self) -> None:
+        self.fs = WandbFileSystem()
 
     def teardown(self):
         del self.fs
