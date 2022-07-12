eb1d8da8d1fdc743d2364e9d90957e2dfdf5c98c
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-07-12 11:25:48 +0000

    🧪 Split `wandbfsspec` and `fsspec` unit tests

--- tests/test_spec.py
+++ tests/test_spec.py
@@ -4,34 +4,37 @@
 import datetime
 from typing import List
 
+import fsspec
 import pytest
 
-from wandbfsspec.core import WandbFile, WandbFileSystem
+from wandbfsspec.core import WandbFile
 
 
-class TestWandbFileSystem:
-    """Test `wandbfsspec.core.WandbFileSystem` class methods."""
+class TestFsspecFileSystem:
+    """Test `fsspec.FileSystem` class methods for `wandbfs`."""
 
     @pytest.fixture(autouse=True)
     def setup_method(self) -> None:
-        self.fs = WandbFileSystem()
+        self.fs = fsspec.filesystem("wandbfs")
 
     def teardown(self):
         del self.fs
 
     def test_ls(self) -> None:
-        """Test `WandbFileSystem.ls` method."""
+        """Test `fsspec.FileSystem.ls` method."""
         files = self.fs.ls(path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/media")
         assert isinstance(files, List)
 
     def test_modified(self) -> None:
+        """Test `fsspec.FileSystem.modified` method."""
         modified_at = self.fs.modified(
             path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/config.yaml"
         )
         assert isinstance(modified_at, datetime.datetime)
 
     def test_open(self) -> None:
-        _file = self.fs.open(
+        """Test `fsspec.FileSystem.open` method."""
+        with self.fs.open(
             path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/config.yaml"
-        )
-        assert isinstance(_file, WandbFile)
+        ) as f:
+            assert isinstance(f, WandbFile)
