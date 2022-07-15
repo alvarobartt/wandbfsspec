9d1b8fc8c2af84d014709e84221b03dc7bd84949
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-07-15 10:13:31 +0000

    🧪 Add `MockRun` dataclass
    It creates a mock run on `wandb` and upload some files so as to later run `wandbfsspec` tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,28 @@
 # Copyright 2022 Alvaro Bartolome, alvarobartt @ GitHub
 # See LICENSE for details.
 
+import os
+
 import pytest
+
+from .utils import MockRun
+
+MOCK_RUN = MockRun(
+    entity=os.getenv("WANDB_ENTITY", "alvarobartt"),
+    project=os.getenv("WANDB_ENTITY", "wandbfsspec-tests"),
+)
 
 
 @pytest.fixture
 def entity() -> str:
-    return "alvarobartt"
+    return MOCK_RUN.entity
 
 
 @pytest.fixture
 def project() -> str:
-    return "resnet-pytorch"
+    return MOCK_RUN.project
 
 
 @pytest.fixture
 def run_id() -> str:
-    return "3boz9td2"
+    return MOCK_RUN.run_id
---
+++ tests/utils.py
@@ -0,0 +1,50 @@
+# Copyright 2022 Alvaro Bartolome, alvarobartt @ GitHub
+# See LICENSE for details.
+
+import os
+from typing import Union
+
+import wandb
+from pydantic.dataclasses import dataclass
+
+PARENT_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.abspath("data")
+FILE_PATH = os.path.join(DATA_PATH, "file.yaml")
+FILES_DIR = os.path.join(DATA_PATH, "files")
+
+
+@dataclass
+class MockRun:
+    entity: str
+    project: str
+    run_name: Union[str, None] = None
+    run_id: Union[str, None] = None
+
+    def __post_init__(self):
+        assert os.getenv("WANDB_API_KEY"), (
+            "In order to connect to the wandb Public API you need to provide the API"
+            " key either via param `api_key`, setting the key in the environment"
+            " variable `WANDB_API_KEY`, or running `wandb login <WANDB_API_KEY>`."
+        )
+
+        assert wandb.run is None
+
+        wandb.init(name=self.run_name, dir=PARENT_PATH)
+        assert wandb.run is not None
+
+        assert wandb.run._entity == self.entity
+        assert wandb.run._project == self.project
+
+        self.run_name = wandb.run._name
+        self.run_id = wandb.run._run_id
+
+        files = wandb.save(glob_str=FILE_PATH, policy="now")
+        files = [os.path.relpath(file, wandb.run.dir) for file in files]
+        assert os.path.basename(FILE_PATH) in files
+
+        files = wandb.save(glob_str=f"{FILES_DIR}/*", base_path=DATA_PATH, policy="now")
+        files = [os.path.relpath(file, f"{wandb.run.dir}/files") for file in files]
+        assert all(file in files for file in os.listdir(FILES_DIR))
+
+        wandb.finish()
+        assert wandb.run is None
