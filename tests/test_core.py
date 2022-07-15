d3d351425bc6abdef1c62a81ec8959b0296cab8d
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-07-15 11:31:32 +0000

    🧪 Use `pytest` fixtures in unit tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,29 +9,27 @@ import pytest
 from wandbfsspec.core import WandbFile, WandbFileSystem
 
 
+@pytest.mark.usefixtures("entity", "project", "run_id")
 class TestWandbFileSystem:
     """Test `wandbfsspec.core.WandbFileSystem` class methods."""
 
     @pytest.fixture(autouse=True)
     def setup_method(self) -> None:
         self.fs = WandbFileSystem()
+        self.path = f"{self.fs.protocol}://{self.entity}/{self.project}/{self.run_id}"
 
     def teardown(self):
         del self.fs
 
     def test_ls(self) -> None:
         """Test `WandbFileSystem.ls` method."""
-        files = self.fs.ls(path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/media")
+        files = self.fs.ls(path=self.path)
         assert isinstance(files, List)
 
     def test_modified(self) -> None:
-        modified_at = self.fs.modified(
-            path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/config.yaml"
-        )
+        modified_at = self.fs.modified(path=f"{self.path}/file.yaml")
         assert isinstance(modified_at, datetime.datetime)
 
     def test_open(self) -> None:
-        _file = self.fs.open(
-            path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/config.yaml"
-        )
+        _file = self.fs.open(path=f"{self.path}/file.yaml")
         assert isinstance(_file, WandbFile)
--- tests/test_spec.py
+++ tests/test_spec.py
@@ -7,34 +7,34 @@ from typing import List
 import fsspec
 import pytest
 
-from wandbfsspec.core import WandbFile
+from wandbfsspec.core import WandbFile, WandbFileSystem
 
 
+@pytest.mark.usefixtures("entity", "project", "run_id")
 class TestFsspecFileSystem:
     """Test `fsspec.FileSystem` class methods for `wandbfs`."""
 
     @pytest.fixture(autouse=True)
     def setup_method(self) -> None:
-        self.fs = fsspec.filesystem("wandbfs")
+        self.fs = fsspec.filesystem(WandbFileSystem.protocol)
+        self.path = (
+            f"{WandbFileSystem.protocol}://{self.entity}/{self.project}/{self.run_id}"
+        )
 
     def teardown(self):
         del self.fs
 
     def test_ls(self) -> None:
         """Test `fsspec.FileSystem.ls` method."""
-        files = self.fs.ls(path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/media")
+        files = self.fs.ls(path=self.path)
         assert isinstance(files, List)
 
     def test_modified(self) -> None:
         """Test `fsspec.FileSystem.modified` method."""
-        modified_at = self.fs.modified(
-            path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/config.yaml"
-        )
+        modified_at = self.fs.modified(path=f"{self.path}/file.yaml")
         assert isinstance(modified_at, datetime.datetime)
 
     def test_open(self) -> None:
         """Test `fsspec.FileSystem.open` method."""
-        with self.fs.open(
-            path="wandbfs://alvarobartt/resnet-pytorch/3boz9td2/config.yaml"
-        ) as f:
+        with self.fs.open(path=f"{self.path}/file.yaml") as f:
             assert isinstance(f, WandbFile)
