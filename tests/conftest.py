0cf96f0568ea7e6817fc8ef3ae599d2682f5da24
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-09-30 12:39:09 +0000

    ✏️ Fix `mypy` type-hints in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ import pytest
 
 from .utils import MockRun
 
-MOCK_RUN = MockRun(
+MOCK_RUN = MockRun(  # type: ignore
     entity=os.getenv("WANDB_ENTITY", "alvarobartt"),
     project=os.getenv("WANDB_PROJECT", "wandbfsspec-tests"),
 )
@@ -25,19 +25,19 @@ def project() -> str:
 
 @pytest.fixture
 def run_id() -> str:
-    return MOCK_RUN.run_id
+    return MOCK_RUN.run_id  # type: ignore
 
 
 @pytest.fixture
 def artifact_type() -> str:
-    return MOCK_RUN.artifact_type
+    return MOCK_RUN.artifact_type  # type: ignore
 
 
 @pytest.fixture
 def artifact_name() -> str:
-    return MOCK_RUN.artifact_name
+    return MOCK_RUN.artifact_name  # type: ignore
 
 
 @pytest.fixture
 def artifact_version() -> str:
-    return MOCK_RUN.artifact_version
+    return MOCK_RUN.artifact_version  # type: ignore
